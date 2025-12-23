41eaa0d32fb32ca6c30d7721265631fb4bf917f6
Aleksandr N Potashev <anipotashev@sberbank.ru>
2025-12-23 14:26:55 +0300

    доработан ippacket (переведен на использование github.com/google/gopacket)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/anpotashev/vpn-common
 
 go 1.24.5
+
+require github.com/google/gopacket v1.1.19
--- pkg/ippacket/ippacket.go
+++ pkg/ippacket/ippacket.go
@@ -1,37 +1,139 @@
 package ippacket
 
 import (
+	"encoding/binary"
 	"fmt"
+	"hash/fnv"
 	"net"
+	"strings"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 )
 
 type IPPacket interface {
-	GetSource() net.IP
-	GetDestination() net.IP
-	GetBytes() []byte
+	Source() net.IP
+	Destination() net.IP
+	Bytes() []byte
+	HashByDestination() (uint16, error)
+	HashBySource() (uint16, error)
 }
 
-type impl []byte
+type impl struct{ gopacket.Packet }
 
-var ErrNotAnIPPacket = fmt.Errorf("not an IP-packet")
+var ErrNoIPv4Layer = fmt.Errorf("no IPv4 layer found")
+var ErrNoTCPLayer = fmt.Errorf("no TCP layer found in packet")
+var ErrNoUDPLayer = fmt.Errorf("no UDP layer found in packet")
+var ErrNoIGMPLayer = fmt.Errorf("no IGMP layer found in packet")
+var ErrUnsupportedIPPacketType = fmt.Errorf("unsupported IP packet type")
 
 func New(data []byte) (IPPacket, error) {
-	// todo расшифровка ??
-	if len(data) < 20 {
-		return nil, ErrNotAnIPPacket
+	packet := gopacket.NewPacket(data, layers.LayerTypeIPv4, gopacket.NoCopy)
+	layer := packet.Layer(layers.LayerTypeIPv4)
+	if layer == nil {
+		return nil, ErrNoIPv4Layer
 	}
-	// todo прочие проверки является ли массив IP пакетом.
-	return impl(data), nil
+	return impl{packet}, nil
 }
 
-func (i impl) GetSource() net.IP {
-	return net.IP(i[12:16])
+func (i impl) HashByDestination() (uint16, error) {
+	layer := i.Layer(layers.LayerTypeIPv4)
+	if layer == nil {
+		return 0, ErrNoIPv4Layer
+	}
+	ip := layer.(*layers.IPv4)
+	switch ip.Protocol {
+	case layers.IPProtocolTCP:
+		tcpLayer := i.Layer(layers.LayerTypeTCP).(*layers.TCP)
+		if tcpLayer == nil {
+			return 0, ErrNoTCPLayer
+		}
+		return hash(ip.DstIP, uint16(tcpLayer.DstPort), byte(layers.IPProtocolTCP)), nil
+	case layers.IPProtocolUDP:
+		udpLayer := i.Layer(layers.LayerTypeUDP).(*layers.UDP)
+		if udpLayer == nil {
+			return 0, ErrNoUDPLayer
+		}
+		return hash(ip.DstIP, uint16(udpLayer.DstPort), byte(layers.IPProtocolUDP)), nil
+	case layers.IPProtocolIGMP:
+		igmpLayer := i.Layer(layers.LayerTypeIGMP)
+		if igmpLayer == nil {
+			return 0, ErrNoIGMPLayer
+		}
+		return hash(ip.DstIP, 0, byte(layers.IPProtocolIGMP)), nil
+	default:
+		return 0, ErrUnsupportedIPPacketType
+	}
 }
 
-func (i impl) GetDestination() net.IP {
-	return net.IP(i[16:20])
+func (i impl) HashBySource() (uint16, error) {
+	layer := i.Layer(layers.LayerTypeIPv4)
+	if layer == nil {
+		return 0, ErrNoIPv4Layer
+	}
+	ip := layer.(*layers.IPv4)
+	switch ip.Protocol {
+	case layers.IPProtocolTCP:
+		tcpLayer := i.Layer(layers.LayerTypeTCP).(*layers.TCP)
+		if tcpLayer == nil {
+			return 0, ErrNoTCPLayer
+		}
+		return hash(ip.SrcIP, uint16(tcpLayer.SrcPort), byte(layers.IPProtocolTCP)), nil
+	case layers.IPProtocolUDP:
+		udpLayer := i.Layer(layers.LayerTypeUDP).(*layers.UDP)
+		if udpLayer == nil {
+			return 0, ErrNoUDPLayer
+		}
+		return hash(ip.SrcIP, uint16(udpLayer.SrcPort), byte(layers.IPProtocolUDP)), nil
+	case layers.IPProtocolIGMP:
+		igmpLayer := i.Layer(layers.LayerTypeIGMP)
+		if igmpLayer == nil {
+			return 0, ErrNoIGMPLayer
+		}
+		return hash(ip.SrcIP, 0, byte(layers.IPProtocolIGMP)), nil
+	default:
+		return 0, ErrUnsupportedIPPacketType
+	}
 }
 
-func (i impl) GetBytes() []byte {
-	return i
+func hash(ip net.IP, port uint16, protocol byte) uint16 {
+	h := fnv.New32a()
+	ipv4 := ip.To4()
+	if ipv4 != nil {
+		h.Write(ipv4)
+	} else {
+		h.Write(ip.To16())
+	}
+	var buf [3]byte
+	binary.BigEndian.PutUint16(buf[:2], port)
+	buf[2] = protocol
+	h.Write(buf[:])
+	return uint16(h.Sum32())
+}
+
+func (i impl) Source() net.IP {
+	layer := i.Layer(layers.LayerTypeIPv4)
+	return layer.(*layers.IPv4).SrcIP.To4()
+}
+
+func (i impl) Destination() net.IP {
+	layer := i.Layer(layers.LayerTypeIPv4)
+	return layer.(*layers.IPv4).DstIP.To4()
+}
+
+func (i impl) Bytes() []byte {
+	return i.Data()
+}
+
+func (i impl) String() string {
+	data := i.Data()
+	strArr := make([]string, min(len(data), 20))
+	for i := range strArr {
+		strArr[i] = fmt.Sprintf("0x%02X", i)
+	}
+	suffix := ""
+	if len(data) > 20 {
+		suffix = "..."
+	}
+	return strings.Join(strArr, " ") + suffix
 }
